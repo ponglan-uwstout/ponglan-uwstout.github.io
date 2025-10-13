f591f2400e292807b60ee0337a24fc7746296102
JALR13 <julia.jalr13@gmail.com>
2025-10-12 23:09:33 -0500

    LAN Page Work, Load Nav Improvement
    Continued initial structuring of the LAN page and sub-foldered it for future file management.  Added an optional parameter to load_nav() to take in a file path modifier based on page location in file structure.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
-function load_nav(){
+function load_nav(level = ""){
     document.getElementById("header").innerHTML = 
     `<div class="flex container">
-        <a href="index.html"><img class="header_logo" src="images/pong_banner-logo_v1.webp"></a>
+        <a href="` + level + `index.html"><img class="header_logo" src="` + level + `images/pong_banner-logo_v1.webp"></a>
         <nav>
             <ul class="flex">
-                <a href="about.html"><li class="header_button">About</li></a>
-                <a href="sponsors.html"><li class="header_button">Sponsors</li></a>
-                <a href="lan.html"><li class="header_button">LAN</li></a>
-                <a href="info.html"><li class="header_button">Events/Info</li></a>
+                <a href="` + level + `about.html"><li class="header_button">About</li></a>
+                <a href="` + level + `sponsors.html"><li class="header_button">Sponsors</li></a>
+                <a href="` + level + `lan.html"><li class="header_button">LAN</li></a>
+                <a href="` + level + `info.html"><li class="header_button">Events/Info</li></a>
             </ul>
         </nav>
     </div> `;
